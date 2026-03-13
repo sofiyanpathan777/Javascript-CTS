86bfd772deec227641551bf82a4092806d0b8ed9
Sofiyan Pathan <65479831+sofiyanpathan777@users.noreply.github.com>
2026-03-13 05:40:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all JS code as newer version
+
+console.log(3+3);
+let name = "sofiyan" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53 
+//bigint
+//string --use " "
+//boolean => true/false
+//null => its standalone value (representation of empty value) 
+//undefined => means value is not assigend yet
+//symbol => unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
