95bf572f6b7c5e9b69639a5f8162f84872ef122e
Sofiyan Pathan <65479831+sofiyanpathan777@users.noreply.github.com>
2026-03-16 11:56:03 +0000

    update

---
+++ 01_basics/05_datatypesSummary.js
@@ -0,0 +1,25 @@
+//Primitive
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt.
+
+const score = 100 //Number
+const scoreValue = 100.3 //Number
+const isLoggedIn = false //Boolean 
+const outsideTemp = null // null
+const userEmail = undefined // Undefined
+const id = Symbol("123") //Symbol
+const anotherId = Symbol("123");
+//note id value can be same but its not equal.
+
+const BigNumber = 122354642343453n //BigInt
+
+//Reference (Non Premitive)
+
+//3 Types: Array, Objects, Functions.
+const heros = ["govinda", "nagarjun", "Shaktiman"];
+//Arrays
+
+let myObj = {
+    name: "sofiyan",
+    age: 22,
+}
