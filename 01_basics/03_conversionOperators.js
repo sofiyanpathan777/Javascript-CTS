f9e10e1a40948cc73aa0417a39e5480af5da37b1
Sofiyan Pathan <65479831+sofiyanpathan777@users.noreply.github.com>
2026-03-16 04:30:49 +0000

    updateed

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -1,2 +1,76 @@
-let score = 33
+/*let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+*/
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+// "" => false
+// "sofiyan" => true
+
+/*let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+*/
+
+
+
+//let i = 0;
+//The do...while statement repeats until a specified condition is false.
+/* do{
+    i+=1;
+    console.log(i);
+}while (i <5); */
+
+//While Statement
+
+/*let  n = 0;
+let x = 0;
+while(n<3){
+    n++;
+    x+=n;
+}
+console.log(x);
+*/
+//*****************Operations***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " sofiyan"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3); //2
+
+// console.log(true);
+
+
+let gameCounter = 100
+++gameCounter;  // prefix and postfix operators 
+console.log(gameCounter); //101
+
+
+
 
