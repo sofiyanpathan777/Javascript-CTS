b8bd5eca5a69fea7f110de77514c883e9e611a94
Sofiyan Pathan <65479831+sofiyanpathan777@users.noreply.github.com>
2026-03-18 04:06:54 +0000

    string

---
+++ 01_basics/06_String.js
@@ -0,0 +1,21 @@
+const name = "sofiyan"
+
+console.log(`Hello my name is ${name.toUpperCase()}`);
+
+const gameName = new String('sofiyan-sp-com')
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+
+const newStringName = "    Sofiyan   "
+console.log(newStringName.trim()); // remove the blanck spaces
+
+const url = "https://sofiyan.com/sofiyan%03pathan"
+
+console.log(url.replace('%03', '-'));
+console.log(gameName.split('-'));
+
+
+
